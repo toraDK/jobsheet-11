a8f67faead87b2b9d38b0eba848ee75c4c9f508d
tora180405 <toradigda@gmail.com>
2023-11-30 22:34:17 +0700

    add file

---
+++ Percobaan3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Percobaan3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Jumlah saldo awal : "); 
+        int saldoAwal = sc.nextInt(); 
+        System.out.print("Lamanya investasi (tahun) :"); 
+        int tahun = sc.nextInt();
+
+        System.out.print ("Jumlah saldo setelah" + tahun + " tahun: "); 
+        System.out.print (hitungLaba (saldoAwal, tahun));
+    }
+
+    static double hitungLaba(double saldo, int tahun){
+        if (tahun == 0) {
+            return saldo;
+        }else{
+            return (1.11 * hitungLaba(saldo, tahun - 1));
+        }
+    }
+}
